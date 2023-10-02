8d19827df7e426d63fae6639b967a712d05e7ce8
gmsumjot <gmsumjot@gotamedia.se>
2023-10-02 16:06:09 +0200

    feat: Added support to fetch images

--- src/services/Objects.ts
+++ src/services/Objects.ts
@@ -28,9 +28,7 @@ const getObject = async (uuid: string) => {
             throw new ObjectsError(`Could not fetch object ${uuid} from OpenContent`)
         }
 
-        const data = await response.text()
-
-        return data
+        return await response.text()
     } catch (error) {
         throw new ObjectsError(`Something went wrong while fetching object ${uuid} from OC`, {
             cause: error as Error
@@ -38,6 +36,44 @@ const getObject = async (uuid: string) => {
     }
 }
 
+const getImage = async (uuid: string) => {
+    const {
+        baseUrl,
+        username,
+        password
+    } = OC_SERVICE_CONFIG
+
+    if (!uuid) {
+        throw new ObjectsError("Missing required uuid for OC service, make sure to pass image uuid")
+    }
+
+    try {
+        const requestParams = {
+            headers: {
+                Authorization: `Basic ${Buffer.from(`${username}:${password}`).toString("base64")}`
+            }
+        };
+        const filesResponse = await fetch(`${baseUrl}/objects/${uuid}/files`, requestParams)
+
+        if (!filesResponse.ok) {
+            throw new ObjectsError(`Could not fetch image-files ${uuid} from OpenContent`)
+        }
+
+        const files = await filesResponse.json()
+        const imageXml = await fetch(
+            `${baseUrl}/objects/${uuid}/files/${files["metadata"][0]["filename"]}`,
+            requestParams
+        )
+
+        return await imageXml.text()
+    } catch (error) {
+        throw new ObjectsError(`Something went wrong while fetching image ${uuid} from OC`, {
+            cause: error as Error
+        })
+    }
+}
+
 export {
-    getObject
+    getObject,
+    getImage
 }
